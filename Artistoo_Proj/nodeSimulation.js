1d4a971bd96ccb69d833e88b9693ba4a2fa9e7ac
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2023-06-01 10:19:36 +0200

    node file

---
+++ Artistoo_Proj/nodeSimulation.js
@@ -0,0 +1,190 @@
+let CPM = require("artistoo-cjs.js")
+
+let config = {
+    // Grid settings
+    ndim : 2,
+    field_size : [650,200],
+
+    // CPM parameters and configuration
+    conf : {
+        // Basic CPM parameters
+        torus : [false,false],				// Should the grid have linked borders?
+        T : 20,								// CPM temperature       
+        // Adhesion parameters:
+        J: [[0,20], [0,0]],
+        // VolumeConstraint parameters
+        LAMBDA_V: [0,50],					// VolumeConstraint importance per cellkind
+        V: [0,200],							// Target volume of each cellkind
+        // PerimeterConstraint parameters
+        LAMBDA_P: [0,2],					// PerimeterConstraint importance per cellkind
+        P : [0,180],						// Target perimeter of each cellkind
+        // ActivityConstraint parameters
+        LAMBDA_ACT : [0,200],				// ActivityConstraint importance per cellkind
+        MAX_ACT : [0,20],					// Activity memory duration per cellkind
+        ACT_MEAN : "geometric"				// Is neighborhood activity computed as a
+                                            // "geometric" or "arithmetic" mean?
+    },  
+
+    // Simulation setup and configuration
+    simsettings : {
+        // Cells on the grid
+        NRCELLS : [10,0],						// Number of cells to seed for all
+                                            // non-background cellkinds.
+        // Runtime etc
+        BURNIN : 0,
+        RUNTIME : 500,
+        RUNTIME_BROWSER : "Inf",
+        
+        // Visualization
+        CANVASCOLOR : "eaecef",
+        CELLCOLOR : ["000000"],
+        ACTCOLOR : [true],					// Should pixel activity values be displayed?
+        SHOWBORDERS : [false],				// Should cellborders be displayed?
+        zoom : 2,							// zoom in on canvas with this factor.
+    }
+}
+
+let sim, meter
+function initialize(){
+let custommethods = {
+        initializeGrid : initializeGrid,
+        buildChannel : buildChannel,
+        drawBelow : drawBelow
+     }
+sim = new CPM.Simulation( config, custommethods )
+sim.Cim = new CPM.Canvas( sim.C, {
+    zoom:sim.conf.zoom, 
+    parentElement : document.getElementById("sim")
+} )
+sim.C.add( new CPM.PreferredDirectionConstraint( {
+	LAMBDA_DIR : [0,100], 
+	DIR : [[0,0],[1,0]]
+} ) )
+sim.helpClasses[ "canvas" ] = true
+meter = new FPSMeter({left:"auto", right:"5px"})
+step()
+}
+
+function step(){
+    sim.step()
+    meter.tick()
+
+if( sim.conf["RUNTIME_BROWSER"] == "Inf" | sim.time+1 < sim.conf["RUNTIME_BROWSER"] ){
+    requestAnimationFrame( step )
+}
+}
+
+function drawBelow(){
+// Set obstacles 
+this.Cim.drawPixelSet( this.channelvoxels, "AAAAAA" ) 
+}
+
+function initializeGrid(){
+    // add the initializer if not already there
+    if( !this.helpClasses["gm"] ){ this.addGridManipulator() }
+    this.buildChannel()
+}
+
+function buildChannel(){ 
+    // FF snel checken om te kijken of we cirkels kunnen maken waar we de cells in kunnen laten spawnen.
+    // 		
+    // van [0, 0] tot [100, 200]
+    //this.channelvoxels = this.gm.makeCircle(  [50, 50], 50, [])
+
+
+    this.channelvoxels = this.gm.makeCircle(  [450,-115], 200, [])
+    this.channelvoxels = this.gm.makeCircle(  [450,315], 200, this.channelvoxels )
+
+    //tumoren
+    //this.channelvoxels = this.gm.makeCircle(  [475,150], 25, this.channelvoxels )
+    //this.channelvoxels = this.gm.makeCircle(  [450,40], 40, this.channelvoxels )
+    //this.channelvoxels = this.gm.makeCircle(  [550,30], 57, this.channelvoxels )
+
+
+    
+    this.channelvoxels = this.gm.makePlane( this.channelvoxels, 1, 0 )
+	let gridheight = this.C.extents[1]
+	this.channelvoxels = this.gm.makePlane( this.channelvoxels, 1, gridheight-1 )
+    this.channelvoxels = this.gm.makePlane( this.channelvoxels, 0, 0)
+    this.channelvoxels = this.gm.makePlane( this.channelvoxels, 0, 649)
+
+    // grid.pixels
+
+    //this.channelvoxels = this.gm.makePlane( this.channelvoxels, 0, 315)
+    //this.channelvoxels = this.gm.makePlane( this.channelvoxels, 0, 550)
+    this.channelvoxels = this.gm.makeCircle(  [265,100], 50, this.channelvoxels )
+    this.channelvoxels = this.gm.makeCircle(  [200,30], 20, this.channelvoxels )
+    this.channelvoxels = this.gm.makeCircle(  [200,170], 20, this.channelvoxels )
+
+    // Add constraint to add collision
+    this.C.add( new CPM.BorderConstraint({
+        BARRIER_VOXELS : this.channelvoxels
+    }) )
+}
+
+/* The following custom methods are used for control buttons on the html page.*/
+
+function startsim(){
+	if( !sim.running ){
+		sim.running = true
+	}
+}
+function stopsim(){
+	sim.running = false
+}
+function seedCell( k ){
+	sim.gm.seedCell(k)
+}
+function seedCells( ncells ){
+	for( let i = 0; i < ncells; i++ ){
+		seedCell( 1 )
+	}
+}
+
+function seedCells(ncells) {
+    // Define Area size, spawning cell width + height dependent on the amount of cells which are spawned.
+    const areaSize = Math.ceil(Math.sqrt(ncells));
+    const cellWidth = (150 / areaSize);
+    const cellHeight = (190 / areaSize);
+
+    // Set limiter on the amount of cells seeded.
+    let count = 0;
+    for (let y = 5; y < 195; y += cellHeight) {
+        for (let x = 20; x < 170; x += cellWidth) {
+            sim.gm.seedCellsInCircle(1, 1, [x + cellWidth / 2, y + cellHeight / 2], 5);
+            count += 1;
+            if (count === ncells) {
+                return;
+            }
+        }
+    }
+}
+
+
+
+
+function killCell(){
+	let t
+	let cells = Object.keys( sim.C.getStat( CPM.PixelsByCell ) )
+	if( cells.length > 0 ){
+		t = cells.pop()
+		for( let cp of sim.C.cellPixels() ){
+			if( cp[1] == t ){
+				sim.C.setpix( cp[0], 0 )
+			}
+		}
+	}
+	sim.C.stat_values = {}
+
+}
+function killAllCells(){
+	let cells = Object.keys( sim.C.getStat( CPM.PixelsByCell ) )
+	if( cells.length == 0 ) return
+	for( let cp of sim.C.cellPixels() ){
+		sim.C.setpix( cp[0], 0 )
+	}
+}
+
+
+
+sim.run()
